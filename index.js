ef7e10428d4e3bcdcc5e95ab64294ea98f5b6d74
BINITA GHALE <ghalebinita54@gmail.com>
2025-07-08 17:54:10 +0545

    changes made

--- index.js
+++ index.js
@@ -1,16 +1,17 @@
 import express from "express";
 import bodyParser from "body-parser";
 import pg from "pg";
+require('dotenv').config();
 
 const app = express();
 const port = 3000;
 
-const db = new pg.Client({
-  user: "postgres",
-  host: "localhost",
-  database: "world",
-  password: "bi54bg54",
-  port: 5432,
+const db = new Client({
+  user: process.env.DB_USER,
+  host: process.env.DB_HOST,
+  database: process.env.DB_NAME,
+  password: process.env.DB_PASSWORD,
+  port: process.env.DB_PORT,
 });
 db.connect();
 
